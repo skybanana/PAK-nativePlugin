f209553a82daae1adeb3d3f2434077191dc09e0a
skybanana <112135946+skybanana@users.noreply.github.com>
2026-08-12 21:44:15 +0900

    test: test generated chart

--- test/RhythmGameDLL.cpp
+++ test/RhythmGameDLL.cpp
@@ -1,7 +1,3 @@
-#include "ChartParser.h"
-
-#include "../src/main.h"
-
 #include <algorithm>
 #include <chrono>
 #include <cmath>
@@ -12,6 +8,10 @@
 #include <string>
 #include <thread>
 #include <vector>
+
+#include "../src/main.h"
+#include "ChartParser.h"
+
 
 #ifdef _WIN32
 #include <conio.h>
@@ -24,12 +24,8 @@ struct PluginApi {
 #ifdef _WIN32
     HMODULE module;
 #endif
-    int (*Initialize)(unsigned int,
-                      unsigned int,
-                      unsigned int,
-                      unsigned int,
-                      unsigned int,
-                      unsigned int);
+    int (*Initialize)(
+        unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
     int (*LoadChart)(const char *);
     int (*StartSession)(void);
     void (*StopSession)(void);
@@ -88,8 +84,7 @@ std::string makeTimingCue(double remainSeconds) {
         filled = 4;
 
     std::string cue;
-    for (int i = 0; i < 4; i++)
-        cue += i < filled ? "[#]" : "[ ]";
+    for (int i = 0; i < 4; i++) cue += i < filled ? "[#]" : "[ ]";
     return cue;
 }
 
@@ -108,8 +103,7 @@ std::string formatJudgeEvent(const JudgeEvent &event) {
     // Builds one display string from a plugin judge event.
     std::ostringstream text;
     text << judgeResultText(event.result) << " (" << std::showpos << std::fixed
-         << std::setprecision(1) << event.errorMs << std::noshowpos << " ms, "
-         << event.noteName;
+         << std::setprecision(1) << event.errorMs << std::noshowpos << " ms, " << event.noteName;
     if (event.result == JudgeResult_Miss && event.detectedMidi != event.targetMidi)
         text << ", MIDI " << event.detectedMidi << "/" << event.targetMidi;
     text << ")";
@@ -138,9 +132,9 @@ void printHud(ClientState *state, const AudioStats &stats) {
         std::cout << " | N finished";
     }
 
-    std::cout << " | Last " << shortenText(state->lastResult, 32) << " | P "
-              << stats.nextNoteIndex << "/" << stats.totalNotes << " DA "
-              << stats.droppedAudioBlocks << " DE " << stats.droppedJudgeEvents << std::flush;
+    std::cout << " | Last " << shortenText(state->lastResult, 32) << " | P " << stats.nextNoteIndex
+              << "/" << stats.totalNotes << " DA " << stats.droppedAudioBlocks << " DE "
+              << stats.droppedJudgeEvents << std::flush;
 }
 
 void printSummary(ClientState *state) {
@@ -261,7 +255,7 @@ void applyJudgeEvent(ClientState *state, const JudgeEvent &event) {
 
 int main(int argc, char *argv[]) {
     unsigned int channels, fs, oDevice = 0, iDevice = 0, iOffset = 0, oOffset = 0;
-    std::string chartPath = "assets/charts/simple_chromatic_001.json";
+    std::string chartPath = "assets/charts/C_Major_Scale.json";
     std::string dllPath = "out/build/ninja-debug/PAKNativePlugin.dll";
 
     // Minimal command-line checking.
@@ -333,8 +327,7 @@ int main(int argc, char *argv[]) {
             break;
 
         JudgeEvent event = {};
-        while (plugin.PollJudgeEvent(&event))
-            applyJudgeEvent(&state, event);
+        while (plugin.PollJudgeEvent(&event)) applyJudgeEvent(&state, event);
 
         if (stats.chartTimeMs < 0.0) {
             printCountdown(stats.chartTimeMs);
